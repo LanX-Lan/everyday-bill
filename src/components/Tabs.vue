e9b44996fd0bea44d7982edf9c482f356649fa16
âBrank-Lan <2055929044@qq.com>
2021-02-24 13:42:45 +0800

    抽取出Tabs.vue组件

---
+++ src/components/Tabs.vue
@@ -0,0 +1,47 @@
+<template>
+  <ol class="tabs">
+    <li :class="{selected: value==='-'}" @click="select('-')">支出</li>
+    <li :class="{selected: value==='+'}" @click="select('+')">收入</li>
+  </ol>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component, Prop} from 'vue-property-decorator';
+
+  @Component
+  export default class Tabs extends Vue {
+    @Prop(String) value!: string
+    select(type: string){
+      this.$emit('update:value',type)
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+  .tabs {
+    display: flex;
+    justify-content: center;
+    padding-bottom: 10px;
+
+    li {
+      padding: 5px 20px;
+      border: 1px solid black;
+      font-weight: 700;
+    }
+
+    li:nth-child(1) {
+      border-radius: 5px 0 0 5px;
+    }
+
+    li:nth-child(2) {
+      border-radius: 0 5px 5px 0;
+    }
+
+    > .selected {
+      background: black;
+      color: #ffffff;
+    }
+  }
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,14 +2,11 @@
   <div>
     <Layout>
       <template :slot="'header'">
-        <ol class="type">
-          <li :class="{selected: type==='-'}" @click="select('-')">支出</li>
-          <li :class="{selected: type==='+'}" @click="select('+')">收入</li>
-        </ol>
+        <Tabs :value.sync="type"/>
       </template>
       <template :slot="'default'">
         <Tags :tag-list="tagList"/>
-        <Note />
+        <Note/>
       </template>
     </Layout>
   </div>
@@ -21,12 +18,13 @@
   import Layout from '@/components/Layout.vue';
   import Tags from '@/components/money/Tags.vue';
   import Note from '@/components/money/Note.vue';
+  import Tabs from '@/components/Tabs.vue';
 
   type Record = {
     tags: Tag[];
   }
   @Component({
-    components: {Note, Tags, Layout}
+    components: {Tabs, Note, Tags, Layout}
   })
   export default class Money extends Vue {
 
@@ -36,8 +34,6 @@
       {id: 2, text: '水果', name: 'fruit'},
       {id: 3, text: '医院', name: 'health'}
     ];
-
-
     select(type: string) {
       this.type = type;
     }
@@ -45,31 +41,6 @@
 </script>
 
 <style lang="scss" scoped>
-  @import "~@/assets/style/helper.scss";
 
-  .type {
-    display: flex;
-    justify-content: center;
-    padding-bottom: 10px;
-
-    li {
-      padding: 5px 20px;
-      border: 1px solid black;
-      font-weight: 700;
-    }
-
-    li:nth-child(1) {
-      border-radius: 5px 0 0 5px;
-    }
-
-    li:nth-child(2) {
-      border-radius: 0 5px 5px 0;
-    }
-
-    > .selected {
-      background: black;
-      color: #ffffff;
-    }
-  }
 
 </style>
