01bb3af0ec71abbd63c0a27b6956d97d0e6d31e3
âBrank-Lan <2055929044@qq.com>
2021-02-25 16:01:56 +0800

    EditLabel.vue的html css部分

---
+++ src/views/EditLabel.vue
@@ -0,0 +1,45 @@
+<template>
+  <Layout :back="true" :nav="false">
+    <div class="bar-wrapper">
+      <span>分类名称：</span>
+      <label>
+        <input placeholder="点击输入"/>
+      </label>
+    </div>
+    <div class="bar-wrapper">
+      <span>收支类型：</span>
+      <span>支出</span>
+    </div>
+    <div class="bar-wrapper">
+      <span>分类图标</span>
+      <span>点击下方图标可选着</span>
+    </div>
+  </Layout>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class EditLabel extends Vue {
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .bar-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  input {
+    border: none;
+    text-align: right;
+    background: transparent;
+  }
+</style>
